265c61ca9fb1f0aa438a8c9ac13bb7142304aa56
s17168 <s17168@LAB214-19>
2020-03-01 14:01:46 +0100

    First Base for project

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //args[0] = "https://www.amazon.com";
+            Console.WriteLine("arg 0 = " + args[0]);
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            Console.WriteLine(response);
+        }
+    }
+}
